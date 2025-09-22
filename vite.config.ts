5e28b8e3142a9eee11fff8fcfcc1e9a79e02eb81
DevIan28 <imarangov@correo.usbcali.edu.co>
2025-09-22 09:05:35 -0500

    fix: set Vite base for GH Pages and use %BASE_URL% in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-const repoName =
-  process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const repo = 'portafolio-ian';
 
 export default defineConfig({
   plugins: [react()],
-  base: repoName ? `/${repoName}/` : '/',
+  base: process.env.GITHUB_ACTIONS ? `/${repo}/` : '/',
   resolve: {
     alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
   },
